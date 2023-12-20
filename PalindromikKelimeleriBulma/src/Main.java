66c729e5f938f599fe9e15355fbb4866c4814e39
tugcegenc <tugceegencc@gmail.com>
2023-12-20 14:41:39 +0300

    Palindromik kelimeleri bulma

--- PalindromikKelimeleriBulma/src/Main.java
+++ PalindromikKelimeleriBulma/src/Main.java
@@ -1,5 +1,23 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Main {
+    static boolean isPalindrom(String str) {
+        String reverse = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverse += str.charAt(i);
+        }
+        return (str.equals(reverse));
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Bir kelime giriniz: ");
+        String c = input.nextLine().toUpperCase();
+
+        if (isPalindrom(c)) {
+            System.out.println("Palindromik kelimedir.");
+        } else {
+            System.out.println("Palindromik kelime değildir.");
+        }
     }
 }
